4a911174e80b91e37a7a517adcb186dce98e5178
meir <contact@flamingo.dev>
2025-05-21 13:54:49 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_version = "1.10.0"
+  
+  cloud {
+    organization = "flamingo.dev"
+    
+    workspaces {
+      name = "shenanigans-tf"
+    }
+  }
+
+  required_providers {
+    source = "aequasi/discord"
+    version = "0.0.4"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+
+variable "DISCORD_TOKEN" {
+  type = string
+  description = "Discord token to manage the server with"
+}
+
