aff4002c6fc9aa356ec70fe69464a9748ec2efb8
meir <contact@flamingo.dev>
2025-05-21 15:20:42 +0200

    add roles

--- roles.tf
+++ roles.tf
@@ -1,1 +1,32 @@
 
+resource discord_role administrator {
+  server_id = discord_server.shenanigans.id
+  name = "Admin"
+  permissions = data.discord_permission.administrators.allow_bits
+  hoist = true
+  mentionable = true
+  position = 1
+}
+
+resource discord_role moderator {
+  server_id = discord_server.shenanigans.id
+  name = "Mod"
+  permissions = data.discord_permission.moderator.allow_bits
+  hoist = true
+  mentionable = true
+  position = 2
+}
+
+resource discord_role og {
+  server_id = discord_server.shenanigans.id
+  name = "OG"
+  permissions = data.discord_permission.og.allow_bits
+  hoist = false
+  mentionable = true
+  position = 3
+}
+
+resource discord_role_everyone everyone {
+  server_id = discord_server.shenanigans.id
+  permissions = data.discord_permission.everyone.allow_bits
+}
