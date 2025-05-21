92f4a2b83ce17c15eabdd228f344a2382bcf08d1
meir <contact@flamingo.dev>
2025-05-21 16:26:03 +0200

    fix role positions

--- roles.tf
+++ roles.tf
@@ -1,42 +1,6 @@
 
-resource discord_role administrator {
-  server_id = discord_server.shenanigans.id
-  name = "Admin"
-  color = 15548997
-  permissions = data.discord_permission.administrators.allow_bits
-  hoist = true
-  mentionable = true
-  position = 22
-}
-
-resource discord_role moderator {
-  server_id = discord_server.shenanigans.id
-  name = "Mod"
-  color = 7419530
-  permissions = data.discord_permission.moderator.allow_bits
-  hoist = true
-  mentionable = true
-  position = 21
-}
-
-resource discord_role og {
-  server_id = discord_server.shenanigans.id
-  name = "OG"
-  color = 15844367
-  permissions = data.discord_permission.og.allow_bits
-  hoist = false
-  mentionable = true
-  position = 20
-}
-
-resource discord_role_everyone everyone {
-  server_id = discord_server.shenanigans.id
-  permissions = data.discord_permission.everyone.allow_bits
-}
-
-variable levels {
-  type = list(string)
-  default = [
+locals {
+  levels = [
     "Level 5",
     "Level 10",
     "Level 15",
@@ -50,24 +14,64 @@ variable levels {
     "Level 55",
     "Level 60",
     "Level 65",
-    "Level 69",
     "Level 70",
-    "Level 75"
+    "Level 75",
+    "Level 80",
+    "Level 85",
+    "Level 90",
+    "Level 95",
+    "MAX LEVEL"
   ]
+
+  list_top = length(local.levels)
+
+  position_og = local.list_top + 1
+  position_moderator = local.position_og + 1
+  position_administrator = local.position_moderator + 1
+}
+
+resource discord_role administrator {
+  server_id = discord_server.shenanigans.id
+  name = "Admin"
+  color = 15548997
+  permissions = data.discord_permission.administrators.allow_bits
+  hoist = true
+  mentionable = true
+  position = local.position_administrator
+}
+
+resource discord_role moderator {
+  server_id = discord_server.shenanigans.id
+  name = "Mod"
+  color = 7419530
+  permissions = data.discord_permission.moderator.allow_bits
+  hoist = true
+  mentionable = true
+  position = local.position_moderator
+}
+
+resource discord_role og {
+  server_id = discord_server.shenanigans.id
+  name = "OG"
+  color = 15844367
+  permissions = data.discord_permission.og.allow_bits
+  hoist = false
+  mentionable = true
+  position = local.position_og
 }
 
 resource random_integer level_color {
-  for_each = toset(var.levels)
+  for_each = toset(local.levels)
   min = 0
   max = 16777215
 }
 
 resource discord_role levels {
-  for_each = toset(var.levels)
+  for_each = toset(local.levels)
   server_id = discord_server.shenanigans.id
   name = each.key
   color = random_integer.level_color[each.key].result
   hoist = false
   mentionable = false
-  position = index(var.levels, each.key) + 1
+  position = index(local.levels, each.key) + 1
 }
