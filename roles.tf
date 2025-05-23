f2afee1ed5c8f8f4c4d702cbc037c80b0b44c38f
meir <contact@flamingo.dev>
2025-05-23 15:32:59 +0200

    fix positions

--- roles.tf
+++ roles.tf
@@ -50,7 +50,7 @@ locals {
   selectable_roles_start = local.level_roles_end
   selectable_roles_end = length(local.selectable_roles) + local.selectable_roles_start
   custom_roles_start = local.selectable_roles_end
-  custom_roles_end = length(local.selectable_roles) + local.custom_roles_start
+  custom_roles_end = length(local.custom_roles) + local.custom_roles_start
 
   position_og = local.custom_roles_end
   position_moderator = local.position_og + 1
